8868c4ebb1c6d7b450a4837bd39ef66ba890c187
ahmet.ozaydin <ahmetozaydn944@gmail.com>
2026-05-02 23:10:21 +0300

    fix

--- src/main/java/com/mulakatim/domain/quiz/QuizSessionQuestionRepository.java
+++ src/main/java/com/mulakatim/domain/quiz/QuizSessionQuestionRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 public interface QuizSessionQuestionRepository extends JpaRepository<QuizSessionQuestion, UUID> {
 
