c0b0114f98e4afc3dd4e27f5502cc7ae29341a9d
ahmet.ozaydin <ahmetozaydn944@gmail.com>
2026-05-02 22:53:11 +0300

    conf

--- src/main/java/com/mulakatim/domain/user/UserRepository.java
+++ src/main/java/com/mulakatim/domain/user/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
-
+ 
     Optional<User> findByGoogleId(String googleId);
 
     boolean existsByEmail(String email);
